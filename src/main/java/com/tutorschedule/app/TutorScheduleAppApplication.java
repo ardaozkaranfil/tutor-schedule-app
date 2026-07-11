9ca273cec3fc6d566d59f7b21315fd91f6258a5c
ardaozkaranfil <arda.ozkaranfil0@gmail.com>
2026-07-12 02:14:23 +0300

    Simplify base package structure

--- src/main/java/com/tutorschedule/app/TutorScheduleAppApplication.java
+++ src/main/java/com/tutorschedule/app/TutorScheduleAppApplication.java
@@ -1,4 +1,4 @@
-package com.tutorschedule.app.tutorscheduleapp;
+package com.tutorschedule.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
