2c36b280d4aeac0a8fa876195626629f92c99133
ardaozkaranfil <arda.ozkaranfil0@gmail.com>
2026-07-12 01:52:19 +0300

    Initial Spring Boot project setup with MySQL configuration

---
+++ src/main/java/com/tutorschedule/app/tutorscheduleapp/TutorScheduleAppApplication.java
@@ -0,0 +1,13 @@
+package com.tutorschedule.app.tutorscheduleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorScheduleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorScheduleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorschedule/app/tutorscheduleapp/TutorScheduleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorschedule.app.tutorscheduleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorScheduleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
